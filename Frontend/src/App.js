3778fd9adbbf2a3e42f50e0c8d86f2c4154637f0
Lucas <Lucas.Altmann@cgi.com>
2022-11-02 11:32:23 +0100

    first API CALL

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/header";
 import Search from './components/search';
-import { useBootstrapBreakpoints } from 'react-bootstrap/esm/ThemeProvider';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = (title) => {
   const [word, setWord] = useState("");
@@ -11,9 +11,19 @@ const App = (title) => {
   const handleSearchSubmit=(e)=> {
     e.preventDefault();
     console.log(word);
-  
+    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=OhbVJaTz_-4dJr8YYaXd42usj0kIm7AqVTbTTfdTjE4`)
+      .then((res) => res.json())
+      .then((data) => {
+          console.log(data);
+      })
+
+      .catch((err) => {
+        console.log(err);
+      })
   }
-  
+
+  console.log(process.env);
+
   return (
     <div>
       <Header title="Images Gallery" />
